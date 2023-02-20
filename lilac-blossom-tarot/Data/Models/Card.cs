ca209d4780bd01cb5dd2eb290aaea8f1fdbebb3d
Anel Guel <anelguel@gmail.com>
2023-02-19 20:02:24 -0500

    added random card generator with JSON details and corresponding image

---
+++ lilac-blossom-tarot/Data/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿namespace lilac_blossom_tarot.Data.Models
+{
+    public class Card
+    {
+        public string? type { get; set; }
+        public string? name_short { get; set; }
+        public string? name { get; set; }
+        public string? value { get; set; }
+        public int? value_int { get; set; }
+        public string? meaning_up { get; set; }
+        public string? meaning_rev { get; set; }
+        public string? desc_up { get; set; }
+    }
+}
---
+++ lilac-blossom-tarot/Data/Models/Cards.cs
@@ -0,0 +1,7 @@
+﻿namespace lilac_blossom_tarot.Data.Models
+{
+    public class Cards
+    {
+        public List<Card>? cards { get; set; }
+    }
+}

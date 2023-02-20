2ea4314e7cd63c73434f25c930e8ecbceb5a5587
Anel Guel <anelguel@gmail.com>
2023-02-20 10:52:08 -0500

    caching WIP

--- lilac-blossom-tarot/Pages/Spreads/DailyPull.cshtml.cs
+++ lilac-blossom-tarot/Pages/Spreads/DailyPull.cshtml.cs
@@ -1,20 +1,28 @@
 using lilac_blossom_tarot.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
 using System.Text.Json;
 
 namespace lilac_blossom_tarot.Pages.Spreads
 {
     public class DailyPullModel : PageModel
     {
+        private readonly IMemoryCache _memoryCache;
+
+        public DailyPullModel(IMemoryCache memoryCache) =>
+            _memoryCache = memoryCache;
+
         public Cards? DeckResults { get; set; }
+
+
         public void OnGet()
         {
             var rawJson = System.IO.File.ReadAllText("wwwroot/deckDescriptions/DeckDescriptions.json");
 
             DeckResults = JsonSerializer.Deserialize<Cards>(rawJson);
 
-         
+           
         }
     }
 }
--- lilac-blossom-tarot/Program.cs
+++ lilac-blossom-tarot/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +16,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();

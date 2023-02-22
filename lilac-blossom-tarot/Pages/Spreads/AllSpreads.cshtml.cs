ce673bdf4f3571f3bf7a5b4a2f47700dc9153bce
Anel Guel <anelguel@gmail.com>
2023-02-22 14:21:25 -0500

    edited Index.cshtml, created AllSpreads architecture

---
+++ lilac-blossom-tarot/Pages/Spreads/AllSpreads.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lilac_blossom_tarot.Pages.Spreads
+{
+    public class AllSpreadsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
